2fc0fa145f9f97b199a9b0ac7a90d2484301404d
ddeenni3 <d.dimitrovv3@gmail.com>
2023-09-26 02:21:32 +0300

    Uploading source code

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -7,29 +7,38 @@ def number_guess(number):
     if number == computer_guess:
         result = 'You guessed it!'
     elif number < computer_guess:
-        result = 'Too low!\nTry again!\n'
+        result = 'Too low!\n'
         if abs(computer_guess - number) < 10:
             result += 'You are getting closer!'
     else:
-        result = 'Too high!\nTry again!\n'
+        result = 'Too high!\n'
         if abs(computer_guess - number) < 10:
-            result += 'You are getting closer!'
+            result += 'You are getting closer!\n '
     return result
 
 
 command = input('Welcome to guess the number! press "enter" to continue or press "q" to quit:')
+
+number_of_guesses = int(input('Please choose the number of guesses for your game: '))
 if command == 'q':
     raise SystemExit('Thanks for playing!')
-
-
 elif command == '':
-    while True:
-        player_guess = int(input('Enter the number between 1 and 100: '))
-        if player_guess == computer_guess:
-            print(number_guess(player_guess))
+    while number_of_guesses > 0:
+        player_guess = input('Enter the number between 1 and 100: ')
+        if player_guess.isdigit():
+            player_guess = int(player_guess)
+            number_of_guesses -= 1
+            if player_guess == computer_guess:
+                print(number_guess(player_guess))
+                break
+            else:
+                print(number_guess(player_guess))
+                print(f'Attempts remaining: {number_of_guesses}\n')
+        elif player_guess == 'q':
+            print('Thanks for playing!')
             break
         else:
-            print(number_guess(player_guess))
-            continue
-
+            print('Invalid input!\nPlease try again.')
+    else:
+        print('You ran out of guesses!')
 
